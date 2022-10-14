15144305e7e55cf313db4afffda98bfece46ee24
Diana <miac.stat@mail.ru>
2022-10-14 16:27:22 +0300

    zadacha41

---
+++ zadacha41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите колличество чисел: ");
+int a = int.Parse(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < a; i++)
+{
+    Console.Write($"Введите число {i + 1}: ");
+    int x = int.Parse(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine($"Положительных чисел: {count}");
